2012ea92ceb2cee711244046a73dc6506b777035
Jan Majek <jan.majek@ft.com>
2016-03-29 18:15:02 +0100

    Test JawsDB

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo 'YEAH';
+echo getenv('JAWSDB_URL');
+
+//$dbparts = parse_url($url);
+//
+//$hostname = $dbparts['host'];
+//$username = $dbparts['user'];
+//$password = $dbparts['pass'];
+//$database = ltrim($dbparts['path'],'/');
