acca0af50c88a75950d18d17747eba84c8b43f3e
Jan Majek <jan.majek@ft.com>
2016-03-29 18:20:22 +0100

    Test JawsDB

--- index.php
+++ index.php
@@ -15,4 +15,9 @@ $conn = mysqli_connect($hostname, $username, $password, $database);
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connection was successfully established!";
+echo "Connection was successfully established! <hr />";
+
+echo 'h:' . $hostname . '<br />';
+echo 'u:' . $username . '<br />';
+echo 'p:' . $password . '<br />';
+echo 'd:' . $database . '<br />';
